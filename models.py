211e3af90312349498e0682e364a6ca5018dcb50
Joshua Lokken <jrrlokken@gmail.com>
2020-06-23 20:50:29 -0500

    Modified user model

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 def connect_db(app):
@@ -10,31 +12,34 @@ def connect_db(app):
 
 # Models
 
-class User(db.Model, UserMixin):
-    """Site user."""
+class User(db.Model):
+    """Issue Tracker user."""
 
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.Text, nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
     password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Text, nullable=False, default='user')
 
     @classmethod
-    def register(cls, username, password):
+    def register(cls, email, password):
         """Register a user, hashing their password."""
 
         hashed = bcrypt.generate_password_hash(password)
         hashed_utf8 = hashed.decode("utf8")
-        return cls(username=username, password=hashed_utf8)
+        return cls(email=email, password=hashed_utf8)
 
     @classmethod
-    def authenticate(cls, username, password):
+    def authenticate(cls, email, password):
         """Validate that user exists & password is correct.
 
         Return user if valid; else return False.
         """
 
-        user = User.query.filter_by(username=username).one()
+        user = User.query.filter_by(email=email).one()
         if user is not None:
             if bcrypt.check_password_hash(user.password, password):
                 return user

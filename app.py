14ba15f401dffd75ba4477bf37b90188eec14be6
Joshua Lokken <jrrlokken@gmail.com>
2020-06-23 20:50:04 -0500

    Added register and login routes.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-import os
-import functools
+# import os
+# import functools
 
 from flask import Flask, request, render_template, redirect, flash, jsonify
 from flask import session, make_response, g
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin
-from flask_login import login_user, logout_user, current_user, login_required
+# from flask_bcrypt import Bcrypt
+# from flask_login import LoginManager, UserMixin
+# from flask_login import login_user, logout_user, current_user, login_required
 from flask_debugtoolbar import DebugToolbarExtension
-# from models import db, connect_db, User, Issue
+from models import db, connect_db, User
 
 app = Flask(__name__)
 
@@ -18,17 +18,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 debug = DebugToolbarExtension(app)
-bcrypt = Bcrypt()
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "users.login"
+connect_db(app)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = "users.login"
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(user_id)
 
 
 @app.route("/")
 def show_index():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+
+@app.route("/register")
+def show_register_form():
+    return render_template('register_form.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def show_login_form():
+    return render_template('login.html')

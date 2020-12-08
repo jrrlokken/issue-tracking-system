954a639606079962a87a5179722423f4ea7c4400
Joshua Lokken <jrrlokken@gmail.com>
2020-12-08 12:40:42 -0600

    Fixed typo

--- helpers.py
+++ helpers.py
@@ -1,15 +1,15 @@
 def is_assignee(current_user):
     """Is the user an assignee?"""
 
-    if user.role == 'assignee':
+    if current_user.role == 'assignee':
         return True
 
     return False
 
 def is_admin(current_user):
-    """Is the user an assignee?"""
+    """Is the user an admin?"""
 
-    if user.role == 'admin':
+    if current_user.role == 'admin':
         return True
 
     return False

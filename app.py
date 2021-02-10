446693133e73b305daa326cde9be141a3531dd32
Joshua Lokken <jrrlokken@gmail.com>
2021-02-09 21:56:45 -0600

    Turn off sqlalchemy echo

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres:///issue_tracker')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'keepitsecretkeepitsafe!')
 

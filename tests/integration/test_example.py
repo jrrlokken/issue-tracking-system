e7ba4a4912e29536d40e485613c26ca208ce71aa
Joshua Lokken <jrrlokken@gmail.com>
2021-02-19 14:17:47 -0600

    Add login test

--- tests/integration/test_example.py
+++ tests/integration/test_example.py
@@ -6,21 +6,18 @@ from selenium.webdriver.common.by import By
 # options.add_argument('--headless')
 # driver = webdriver.Chrome(options=options)
 
-# def find(self, by, value):
-#     elements = self.driver.find_elements(by, value)
-#     if len(elements) is 1:
-#         return elements[0]
-#     else:
-#         return elements
 driver = webdriver.Chrome()
 
-driver.get('http://localhost:5000/')
-element = driver.find_element(By.CSS_SELECTOR, '.display-4')
-print(element.text)
-driver.close()
+# driver.get('http://localhost:5000/')
+# element = driver.find_element(By.CSS_SELECTOR, '.display-4')
+# print(element.text)
+# driver.close()
 
-driver.get('https://flask-its.herokuapp.com/login')
+driver.get('http://localhost:5000/login')
 email = driver.find_element(By.ID, 'email')
 password = driver.find_element(By.ID, 'password')
-email.send_keys('testuser@example.com')
+form = driver.find_element(By.ID, 'login-form')
+button = driver.find_element(By.CSS_SELECTOR, '#login-button')
+email.send_keys('testuser2@example.com')
 password.send_keys('password')
+button.click()
